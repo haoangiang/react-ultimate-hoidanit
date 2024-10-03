01754e6d4832f77045ef64f6de3abfd9a487b024
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-03 22:48:00 +0700

    video 33

---
+++ src/components/todo/todoData.jsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+    return (
+        <div className="todo-data">
+            <div className="todo-item">Learning React</div>
+            <div className="todo-item">Watching Youtube</div>
+        </div>
+    )
+}
+
+export default TodoData;
